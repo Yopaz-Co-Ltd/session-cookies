5494dc7f1c445a3e6f2cebb8e259b94f7d508dc9
yopaz-khainh <nguyenhuukhai1303@gmail.com>
2025-08-07 00:52:48 +0700

    feat: add slide set cookie

--- login_demo_with_cookie/public/page1.php
+++ login_demo_with_cookie/public/page1.php
@@ -1,18 +1,13 @@
 <?php
 $users = require __DIR__ . '/../data/users.php';
-
 $user_id = $_COOKIE['user_id'] ?? null;
+
 if (!$user_id || !isset($users['by_id'][$user_id])) {
     header("Location: login.php?error=" . urlencode("Vui lòng đăng nhập lại."));
     exit;
 }
 
 $username = $users['by_id'][$user_id];
-
-$msg = '';
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $msg = $_POST['message'] ?? '';
-}
 ?>
 
 <!DOCTYPE html>
@@ -26,22 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <div class="container">
     <div class="login-form">
       <h2>Trang 1</h2>
-
-      <p>Xin chào <strong><?php echo $username ?></strong></p>
-
-      <form method="POST" style="margin-bottom: 20px; margin-top: 20px;">
-        <label for="message">Gửi nội dung:</label>
-        <textarea name="message" id="message" rows="4" cols="35"></textarea>
-        <button style="margin-top: 20px;" type="submit">Gửi</button>
-      </form>
-
-      <?php if ($msg): ?>
-        <h4>Nội dung bạn vừa gửi:</h4>
-        <div style="background: #eee; padding: 10px; border-radius: 5px; margin-top: 10px;">
-          <?php echo $msg; ?>
-        </div>
-      <?php endif; ?>
-
+      <p>Chào bạn <strong><?php echo htmlspecialchars($username); ?></strong>, đây là nội dung Trang 1.</p>
       <div style="margin-top: 20px;">
         <a href="dashboard.php"><button>Quay về Dashboard</button></a>
       </div>
